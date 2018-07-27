548e61c1c1dcf3b2cb4f107d01ee11c3c25d194d
pierre-saikaly <saikalypierre@gmail.com>
2018-07-27 11:32:48 +0200

    Cleaned up the code and dispatched in different functions

--- SenseorDataExp.py
+++ SenseorDataExp.py
@@ -14,6 +14,8 @@
 import time
 from datetime import datetime, date, time
 import os
+import sys
+import glob
 import csv
 import numpy as np
 import math
@@ -34,28 +36,24 @@ def AvgXSeconds(Sensx_Data, seconds_add):
 	
 	
 
-def SensSort(Sensx_DataRaw):
+def SensSort(csvfile):
 	"""
 		Sort raw SenseorData based on their index. 
 		Input :
+			- Sensx_DataRaw_np : Outputs
 			- SensData_Raw : Senseor data coming from csv file define as (timestamp, temperature)
 		Output :
-			- One 
+			- None
 		Used in : Main
-	"""	
-
-
-def main():
-
-	#Sens0_DataRaw = [0,0,0,0,0]
-	#Sens1_DataRaw = [0,0,0,0,0]
-	#Sens2_DataRaw = [0,0,0,0,0]
+	"""
+	# Local variables :
+	# -----------------
 	i = 0
 	Sens0_DataRaw_np = np.zeros(shape=(0,2))
 	Sens1_DataRaw_np = np.zeros(shape=(0,2))
 	Sens2_DataRaw_np = np.zeros(shape=(0,2))
-	
-	with open('2017-11_000000177_measures.csv', newline='') as f:
+
+	with open(csvfile, newline='') as f:
 		reader = csv.reader(f, delimiter=';')
 		next(reader, None) # skip header
 		for row in reader:
@@ -68,50 +66,46 @@ def main():
 				if row[3] == '1':
 					print('yeah1')
 					Sens1_DataRaw_np = np.vstack((Sens1_DataRaw_np, [float(row[0]), float(row[4])]))
-					#Sens1_DataRaw_np = np.vstack((Sens1_DataRaw_np, [np.datetime64(datetime.strptime(row[1], "%d/%m/%Y %H:%M:%S")), float(row[4])]))
 				if row[3] == '2':
 					print('yeah2')
 					Sens2_DataRaw_np = np.vstack((Sens2_DataRaw_np, [float(row[0]), float(row[4])]))
-					#Sens2_DataRaw_np = np.vstack((Sens2_DataRaw_np, [np.datetime64(datetime.strptime(row[1], "%d/%m/%Y %H:%M:%S")), float(row[4])]))
-	#print(Sens0_DataRaw)
-	#print(Sens1_DataRaw)
-	#print(Sens2_DataRaw)
-	#print(Sens0_DataRaw_np[0,0])
-	#date_start = np.datetime64(Sens0_DataRaw_np[0,0],'D')
-	#date_end   = np.datetime64(Sens0_DataRaw_np[1000,0D') + np.timedelta64(1,'D')
-	#date_cond  = np.datetime64(date_start, 'D')
+
+	return Sens0_DataRaw_np, Sens1_DataRaw_np, Sens2_DataRaw_np				
+
+def main():
+
+	i = 0
+	Sens0_np = np.zeros(shape=(0,2))
+	Sens1_np = np.zeros(shape=(0,2))
+	Sens2_np = np.zeros(shape=(0,2))
+	
+	for file_csv in glob.glob('*.csv'):
+		[Sens0_out, Sens1_out, Sens2_out] = SensSort(file_csv)
+		Sens0_np = np.vstack((Sens0_np, Sens0_out))
+		Sens1_np = np.vstack((Sens1_np, Sens1_out))
+		Sens2_np = np.vstack((Sens2_np, Sens2_out))
+	print(Sens0_np)
 
 	day = 86400
-	paramater  = 600
+	time_parameter  = 600
 	Sens0_DataAvg_np = np.zeros(shape=(0,2))
-	date_start = (Sens0_DataRaw_np[0,0] // day) * day
-	date_end   = (Sens0_DataRaw_np[len(Sens0_DataRaw_np)-1,0] // day + 1) *day
+	date_start = (Sens0_np[0,0] // day) * day
+	date_end   = (Sens0_np[len(Sens0_np)-1,0] // day + 1) *day
 	date_cond  = date_start
 	print("date_cond = ", date_cond)
-	print("premiere_date = ", Sens0_DataRaw_np[0,0])
-	print(Sens0_DataRaw_np[0,0] - date_cond)
+	print("premiere_date = ", Sens0_np[0,0])
+	print(Sens0_np[0,0] - date_cond)
 
 
 	while date_cond < date_end:
-		temp_values = np.where(Sens0_DataRaw_np[:,0]//600 == date_cond//600 ) 
+		temp_values = np.where(Sens0_np[:,0]//time_parameter == date_cond//time_parameter ) 
 		#print(temp_values)
-		temp_avg    = np.average(Sens0_DataRaw_np[temp_values,1])
+		temp_avg    = np.average(Sens0_np[temp_values,1])
 		#print(temp_avg)
 		if temp_avg == temp_avg:
 			Sens0_DataAvg_np = np.vstack((Sens0_DataAvg_np, [date_cond,temp_avg]))
-		date_cond  += 600
+		date_cond  += time_parameter
 
-	#print(Sens0_DataAvg_np)
-
-		# #MOY = 0
-		# n = 0
-		# for i in range(0,len(Sens0_DataRaw_np)):
-		# 	if 600>(Sens0_DataRaw_np[i,0] - date_cond):
-		# 		MOY = MOY + Sens0_DataRaw_np[i,1]
-		# 		n = n + 1
-		# #if n != 0:
-		# 	#print(MOY/n)
-		# date_cond = date_cond + 600
 
 	Datets = []
 	for i in range(0,len(Sens0_DataAvg_np)):
@@ -119,12 +113,6 @@ def main():
 
 	plt.plot(Datets,Sens0_DataAvg_np[:,1])
 	plt.show()
-		
-		
-	#Sens0_DataRaw_np = np.array(Sens0_DataRaw).astype("float")
-	
-	#dt = datetime.strptime("21/11/06 16:30", "%d/%m/%y %H:%M")
-
 
 if __name__ == '__main__': 
 	main()
